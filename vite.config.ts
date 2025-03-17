60b4ba67369afe6a2700626784f3eb0485645228
Egil Eskilsson <egil@egileskilsson.se>
2025-03-17 16:24:40 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,5 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-  'base': '/jazzy-quiz/',
-  //css: {
-  //  preprocessorOptions: {
-  //   scss: {
-  //      additionalData: `@import "./src/scss/abstracts/variables/_mx-*";`
-  //    }
-  //  }
-  // }
+  'base': '/jazzy-quiz/'
 });
